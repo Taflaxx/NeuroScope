4f045f76a9f74916e1e14eb502cbfc52c12c914c
Taflaxx <13418652+Taflaxx@users.noreply.github.com>
2025-07-05 14:33:49 +0200

    improve websocket url handling

--- NeuroScope/NeuroScope.cs
+++ NeuroScope/NeuroScope.cs
@@ -11,30 +11,27 @@ public class NeuroScope : OWML.ModHelper.ModBehaviour
 	public void Awake()
 	{
 		Instance = this;
-		// You won't be able to access OWML's mod helper in Awake.
-		// So you probably don't want to do anything here.
-		// Use Start() instead.
 	}
 
 	public void Start()
 	{
-		// Starting here, you'll have access to OWML's mod helper.
-		ModHelper.Console.WriteLine($"My mod {nameof(NeuroScope)} is loaded!", MessageType.Success);
-
+		ModHelper.Console.WriteLine($"{nameof(NeuroScope)} is loaded!", MessageType.Success);
 		new Harmony("Taflax.NeuroScope").PatchAll(Assembly.GetExecutingAssembly());
 
-		// Example of accessing game code.
-		OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
-		LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
-
-		System.Environment.SetEnvironmentVariable("NEURO_SDK_WS_URL", "ws://localhost:8000");
-		NeuroSdk.NeuroSdkSetup.Initialize("Outer Wilds");
-	}
-
-	public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
-	{
-		if (newScene != OWScene.SolarSystem) return;
-		ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+		if (!ModHelper.Config.GetSettingsValue<string>("Websocket URL").Equals(""))
+		{
+			System.Environment.SetEnvironmentVariable("NEURO_SDK_WS_URL", ModHelper.Config.GetSettingsValue<string>("Websocket URL"));
+		}
+		if (System.Environment.GetEnvironmentVariable("NEURO_SDK_WS_URL") == null)
+		{
+			ModHelper.Console.WriteLine("No Websocket URL set", MessageType.Error);
+		}
+		else
+		{
+			ModHelper.Console.WriteLine($"Websocket URL set to '{System.Environment.GetEnvironmentVariable("NEURO_SDK_WS_URL")}'", MessageType.Info);
+			NeuroSdk.NeuroSdkSetup.Initialize("Outer Wilds");
+		}
+		
 	}
 }
 
